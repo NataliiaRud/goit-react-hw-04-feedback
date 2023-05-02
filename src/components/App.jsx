c6d4d0f2fc5a9b15e2efeb6f166bafe57b5161e3
Nataliia_Rud <nrud1981@gmail.com>
2023-05-02 13:11:16 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Component } from "react";
+
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
 import { Section } from "./Section/Section";
